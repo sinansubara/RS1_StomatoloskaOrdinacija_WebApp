c423b13af5caa191477114ad036413090a640e0d
Sinan Šubara <sinan.subara@gmail.com>
2020-01-07 03:39:22 +0100

    init update

---
+++ StomatoloskaOrdinacija.Data/MyContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StomatoloskaOrdinacija.Data
+{
+    public class MyContext:DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> x) : base(x)
+        {
+
+        }
+    }
+}
--- StomatoloskaOrdinacija.Web/Controllers/HomeController.cs
+++ StomatoloskaOrdinacija.Web/Controllers/HomeController.cs
@@ -1,33 +1,23 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using StomatoloskaOrdinacija.Web.Models;
+using StomatoloskaOrdinacija.Data;
 
 namespace StomatoloskaOrdinacija.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private MyContext _db; 
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, MyContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }

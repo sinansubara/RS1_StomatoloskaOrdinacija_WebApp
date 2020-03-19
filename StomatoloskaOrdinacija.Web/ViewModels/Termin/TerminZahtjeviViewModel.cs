13930605338cc788ae8e33de32d1a44169562689
Sinan Šubara <sinan.subara@gmail.com>
2020-03-19 01:42:56 +0100

    118. Prihvatanje/odbijanje termina

---
+++ StomatoloskaOrdinacija.Web/ViewModels/Termin/TerminZahtjeviViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StomatoloskaOrdinacija.Web.ViewModels.Termin
+{
+    public class TerminZahtjeviViewModel
+    {
+        public int TerminId { get; set; }
+        public int PacijentId { get; set; }
+        public string Pacijent { get; set; }
+        public DateTime Datum { get; set; }
+        public DateTime Vrijeme { get; set; }
+        public string Razlog { get; set; }
+        public string Hitan { get; set; }
+        public string Adresa { get; set; }
+        public string JMBG { get; set; }
+        public string Mobitel { get; set; }
+        public string Odobren { get; set; }
+    }
+}
